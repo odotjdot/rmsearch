e277f98977c28e08e740dd55351f887f8de49df5
OJ Smith <oj.smith@funkmedia.net>
2021-05-01 18:16:07 -0700

    added sass and animations

--- pages/character/[id]/index.js
+++ pages/character/[id]/index.js
@@ -3,18 +3,6 @@ import Head from 'next/head';
 
 const defaultEndPoint = 'https://rickandmortyapi.com/api/character';
 
-export async function getServerSideProps({ query }) {
-  const { id } = query;
-  const res = await fetch(`${defaultEndPoint}/${id}`);
-  const data = await res.json();
-
-  return {
-    props: {
-      data,
-    }
-  }
-}
-
 export default function Character({ data }) {
   const { name, image, gender, location, origin, species, status } = data;
   console.log(data)
@@ -29,6 +17,13 @@ export default function Character({ data }) {
         { name }
         </h1>
 
+        <p>
+          <Link href="/">
+            <a>
+              Back To All Characters
+            </a>
+          </Link>
+        </p>
 
         <ul className="grid">
           <li className="card">
@@ -58,168 +53,22 @@ export default function Character({ data }) {
           </li>
           
         </ul>
-        <p>
-          <Link href="/">
-            <a>
-              Back To All Characters
-            </a>
-          </Link>
-        </p>
+
       </main>
 
-      <footer>
-
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-
-          
-        }
-        .grid li {
-          list-style: none;
-        }
-        .grid ul {
-          margin: 0;
-          padding: 0;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 39%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .card img {
-          width: 100%;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      
     </div>
   )
+}
+
+export async function getServerSideProps({ query }) {
+  const { id } = query;
+  const res = await fetch(`${defaultEndPoint}/${id}`);
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    }
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,10 @@
 import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
+
 
 const defaultEndPoint = 'https://rickandmortyapi.com/api/character';
-
-export async function getServerSideProps() {
-  const res = await fetch(defaultEndPoint);
-  const data = await res.json();
-
-  return {
-    props: {
-      data,
-    }
-  }
-}
 
 export default function Home({ data }) {
 
@@ -26,7 +17,6 @@ export default function Home({ data }) {
 
   const { current } = page;
 
-  console.log(page)
 
   useEffect(()=> {
     if ( current === defaultEndPoint ) return;
@@ -46,7 +36,6 @@ export default function Home({ data }) {
       }
 
       updateResults(prev => {
-        console.log('prev =======' , prev)
         return [
           ...prev,
           ...nextData.results,
@@ -89,30 +78,69 @@ export default function Home({ data }) {
       </Head>
 
       <main>
-        <h1 className="title">
-          Rick And Morty Wiki
-        </h1>
-
-
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: .8,
+              opacity: 0,
+            },
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: .1,
+              }
+            }
+          }}
+        >
+          <h1 className="title">
+            Rick And Morty Wiki
+          </h1>
+        </motion.div>
+        <p>
         <form className="search" onSubmit={handleOnSubmitSearch}>
           <input name="query" type="search" />
           <button>Search</button>
         </form>
-
+        </p>
 
         <ul className="grid">
           {results.map(result => {
             const { id, name, image } = result;
 
             return (
-              <li className="card" key={id}>
+              <motion.li 
+                className="card" 
+                key={id}
+                whileHover={{
+                  position: 'relative',
+                  zIndex: 1,
+                  background: 'white',
+                  scale: [1, 1.4, 1.6, 1.2],
+                  rotate: [0, 10, -10, 0],
+                  filter: [
+                    'hue-rotate(0) contrast(100%)',
+                    'hue-rotate(360deg) contrast(200%)',
+                    'hue-rotate(45deg) contrast(300%)',
+                    'hue-rotate(0) contrast(100%)',
+                  ],
+                  transition: {
+                    duration: .2,
+                  }
+                }}
+              >
                 <Link href="/character/[id]" as={`/character/${id}`}>
                   <a>
-                    <img src={image} alt={`${name}-thumb`}/>
+                    <img
+                      src={image}
+                      alt={`${name}-thumb`}
+                    />
                     <h3>{name}</h3>
                   </a>
                 </Link>
-              </li>
+              </motion.li>
             )
           })}
           
@@ -120,159 +148,19 @@ export default function Home({ data }) {
         <button onClick={handleLoadMore}>Load More</button>
       </main>
 
-      <footer>
 
-      </footer>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-
-          
-        }
-        .grid li {
-          list-style: none;
-        }
-        .grid ul {
-          margin: 0;
-          padding: 0;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 39%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .card img {
-          width: 100%;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(defaultEndPoint);
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    }
+  }
 }
